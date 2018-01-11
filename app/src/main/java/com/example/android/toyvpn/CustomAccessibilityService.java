e7c0d505ee00bb797bcc15df5d611271db5cec80
Changli Tan <changlitan@sf-express.com>
2018-01-11 21:06:03 +0800

    Add accessibility service

---
+++ app/src/main/java/com/example/android/toyvpn/CustomAccessibilityService.java
@@ -0,0 +1,18 @@
+package com.example.android.toyvpn;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class CustomAccessibilityService extends AccessibilityService {
+
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
